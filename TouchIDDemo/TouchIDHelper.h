ecab6fd32265ae8dea95a91ed34c6919b2e47efb
54fanqie <2917545349@qq.com>
2018-03-05 16:11:57 +0800

    first commit

---
+++ TouchIDDemo/TouchIDHelper.h
@@ -0,0 +1,13 @@
+//
+//  TouchIDHelper.h
+//  TouchIDDemo
+//
+//  Created by 番茄 on 2017/11/17.
+//  Copyright © 2017年 番茄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TouchIDHelper : NSObject
+
+@end
---
+++ TouchIDDemo/TouchIDHelper.m
@@ -0,0 +1,13 @@
+//
+//  TouchIDHelper.m
+//  TouchIDDemo
+//
+//  Created by 番茄 on 2017/11/17.
+//  Copyright © 2017年 番茄. All rights reserved.
+//
+
+#import "TouchIDHelper.h"
+
+@implementation TouchIDHelper
+
+@end
